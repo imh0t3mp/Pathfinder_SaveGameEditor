f522c80bddb9a521c697326280450ee0313c8359
Tobias Menzel <tobias.menzel@posteo.de>
2018-10-15 00:39:56 +0200

    Add two buttons
    Button one is to load the save file
    Button two is to quit the application(tested)

--- Application/MainWindow.py
+++ Application/MainWindow.py
@@ -15,12 +15,18 @@ import tkinter as tk
 
 class MainWindow(tk.Frame):
     """Class for the application main window"""
+
     # Initialize.
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
-        self.create_widgets()
 
-    # Create widgets for the main window.
-    def create_widgets(self):
-        pass
+        # Add button action load file.
+        self.btn_load_savefile = tk.Button(self)
+        self.btn_load_savefile['text'] = 'Load save'
+        self.btn_load_savefile.pack(side='top')
+
+        # Add button action quit application.
+        self.btn_quit = tk.Button(self, fg='red', command=master.destroy)
+        self.btn_quit['text'] = 'Quit Program'
+        self.btn_quit.pack(side='bottom')
