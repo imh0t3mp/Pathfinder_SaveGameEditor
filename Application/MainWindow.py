0592ad57ed3d71ef34b763bb9df246c56297d451
Tobias Menzel <tobias.menzel@posteo.de>
2018-10-15 22:52:29 +0200

    Add save file button
    Add new button to gui, for saving the file.
    And renamed the text for load save to load file.

--- Application/MainWindow.py
+++ Application/MainWindow.py
@@ -23,10 +23,15 @@ class MainWindow(tk.Frame):
 
         # Add button action load file.
         self.btn_load_savefile = tk.Button(self)
-        self.btn_load_savefile['text'] = 'Load save'
+        self.btn_load_savefile['text'] = 'Load file'
         self.btn_load_savefile.pack(side='top')
 
         # Add button action quit application.
         self.btn_quit = tk.Button(self, fg='red', command=master.destroy)
         self.btn_quit['text'] = 'Quit Program'
         self.btn_quit.pack(side='bottom')
+
+        # Add button action save file.
+        self.btn_save_file = tk.Button(self)
+        self.btn_save_file['text'] = 'Save file'
+        self.btn_save_file.pack(side='top')
