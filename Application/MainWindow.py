d6ede2fd3d53dd2a3715d8f5e2fa1e7f927576c5
Tobias Menzel <tobias.menzel@posteo.de>
2018-10-15 23:34:42 +0200

    Worked on MainWindow Class
    import new module to add a filedialog.
    add a menu with a filemenu & a helpmenu.
    add a staticmethod to open a file.

--- Application/MainWindow.py
+++ Application/MainWindow.py
@@ -8,6 +8,7 @@
 
 # Import modules for the MainWindow class.
 import tkinter as tk
+from tkinter.filedialog import askopenfilename
 
 
 # Define class for the main window.
@@ -15,16 +16,48 @@ import tkinter as tk
 
 class MainWindow(tk.Frame):
     """Class for the application main window"""
-
     # Initialize.
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
 
+        # # # # # # # #
+        #   MENU      #
+        # # # # # # # #
+
+        # Initialize menu & config.
+        self.menu = tk.Menu(master)
+        self.master.config(menu=self.menu)
+
+        # Add Menus.
+        self.filemenu = tk.Menu(self.menu)
+        self.helpmenu = tk.Menu(self.menu)
+
+        # Add options for the menus.
+        self.menu.add_cascade(label='File', menu=self.filemenu)
+        self.menu.add_cascade(label='Help', menu=self.helpmenu)
+
+        # Add actions for the filemenu.
+        self.filemenu.add_command(label='Open', command=self.on_open_file)
+        self.filemenu.add_command(label='Save')
+        self.filemenu.add_command(label='Quit', command=master.destroy)
+
+        # Add actions for the helpmenu.
+        self.helpmenu.add_command(label='Readme')
+        self.helpmenu.add_command(label='About')
+
+        # # # # # # # #
+        #   MENU END  #
+        # # # # # # # #
+
+        # # # # # # # # # # #
+        # CONTROL ELEMENTS  #
+        # # # # # # # # # # #
+
         # Add button action load file.
-        self.btn_load_savefile = tk.Button(self)
-        self.btn_load_savefile['text'] = 'Load file'
-        self.btn_load_savefile.pack(side='top')
+        self.btn_load_file = tk.Button(self, command=self.on_open_file)
+        self.btn_load_file['text'] = 'Load file'
+        self.btn_load_file.pack(side='top')
 
         # Add button action quit application.
         self.btn_quit = tk.Button(self, fg='red', command=master.destroy)
@@ -35,3 +68,11 @@ class MainWindow(tk.Frame):
         self.btn_save_file = tk.Button(self)
         self.btn_save_file['text'] = 'Save file'
         self.btn_save_file.pack(side='top')
+
+    # Define static methods.
+    @staticmethod
+    def on_open_file():
+        """Opens a file dialog"""
+        file = askopenfilename()
+        print("Your choosen file: {file}".format(file=file))  # Test Test Test
+
